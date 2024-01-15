f678060b93400ccb05554640b41c21f4e8b830a2
Awade Abhijit <155374509+coder-abhii-007@users.noreply.github.com>
2024-01-15 12:29:20 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,18 @@
+Class Prime{
+  Public Static void main(String []Args){
+    int a=10;
+    int cout=0;
+    for(int i=0;i<=a;i++){
+    if (a%i==0){
+        cout++;
+      }
+    }
+      if(cout==2){
+        System.out.pritln("it is a Prime Number");
+      }
+      else
+        System.out.Println("Not a Prime Number");
+    }
+  }
+}
+      
