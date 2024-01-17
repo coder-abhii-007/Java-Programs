d52c036b6061c1e5a99364ec3154e2e4f95fe01f
Awade Abhijit <155374509+coder-abhii-007@users.noreply.github.com>
2024-01-17 21:14:08 +0530

    Create ArraySort.java

---
+++ ArraySort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 6};
+        Arrays.sort(numbers);
+        System.out.println("Sorted Array: " + Arrays.toString(numbers));
+    }
+}
